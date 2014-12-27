838bb19cda10cfdcaa972663b701cede13a8ec0d
Patrick Debois <Patrick.Debois@jedi.be>
2014-12-27 19:53:47 +0100

    specify EU bucket location

--- scripts-ci/s3_upload.sh
+++ scripts-ci/s3_upload.sh
@@ -6,9 +6,10 @@ S3_BUCKET="devopsdays.org"
 ################################################################################
 # Upload S3
 ################################################################################
-echo "Uploading new assets to s3 bucket $S3_BUCKET"
+echo "Uploading new assets to s3 bucket $S3_BUCKET using account $S3_ACCESS_KEY"
 echo "[default]" > ~/.s3cfg
 echo "access_key=$S3_ACCESS_KEY" >> ~/.s3cfg
 echo "secret_key=$S3_SECRET_KEY" >> ~/.s3cfg
 
-s3cmd sync --acl-public --progress site/content/ s3://$S3_BUCKET
+s3cmd sync --acl-public --progress site/content/ s3://$S3_BUCKET --bucket-location=EU
+
