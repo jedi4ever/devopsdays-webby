3e78cc10f3ebb86f870a200d3553403687af8f1b
Patrick Debois <Patrick.Debois@jedi.be>
2014-12-27 17:14:38 +0100

    auto upload to s3

---
+++ scripts-ci/s3_upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+S3_BUCKET="devopsdays.org"
+
+################################################################################
+# Upload S3
+################################################################################
+echo "Uploading new assets to s3 bucket $S3_BUCKET"
+echo "[default]" > ~/.s3cfg
+echo "access_key=$S3_ACCESS_KEY" >> ~/.s3cfg
+echo "secret_key=$S3_SECRET_KEY" >> ~/.s3cfg
+
+s3cmd sync --acl-public --progress site/content/ s3://$S3_BUCKET
