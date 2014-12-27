60a642086c6e44cba9b95ea327c4ffbc77f3b28e
Patrick Debois <Patrick.Debois@jedi.be>
2014-12-27 20:09:52 +0100

    install s3cmd 1.5.0-beta1

--- scripts-ci/s3_upload.sh
+++ scripts-ci/s3_upload.sh
@@ -6,10 +6,14 @@ S3_BUCKET="devopsdays.org"
 ################################################################################
 # Upload S3
 ################################################################################
-echo "Uploading new assets to s3 bucket $S3_BUCKET using account $S3_ACCESS_KEY"
+echo "Uploading new assets to s3 bucket $S3_BUCKET using circleci-devopsdays account"
 echo "[default]" > ~/.s3cfg
 echo "access_key=$S3_ACCESS_KEY" >> ~/.s3cfg
 echo "secret_key=$S3_SECRET_KEY" >> ~/.s3cfg
 
-s3cmd sync --acl-public --progress site/content/ s3://$S3_BUCKET --bucket-location=EU
+s3cmd_version=1.5.0-beta1
+echo "download s3cmd $s3cmd_version"
+curl -O -L https://github.com/s3tools/s3cmd/archive/v$s3cmd_version.tar.gz
+tar xzf v$s3cmd_version.tar.gz
 
+s3cmd-$s3cmd_version/s3cmd sync --acl-public --progress site/content/ s3://$S3_BUCKET --bucket-location=EU
