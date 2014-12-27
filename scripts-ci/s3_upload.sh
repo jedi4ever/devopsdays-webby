b28b739646abb5d3b3d07afc3261ca37c5a4d2c1
Patrick Debois <Patrick.Debois@jedi.be>
2014-12-27 20:53:41 +0100

    it's output dir that goes to s3

--- scripts-ci/s3_upload.sh
+++ scripts-ci/s3_upload.sh
@@ -16,6 +16,4 @@ echo "download s3cmd $s3cmd_version"
 curl -O -L https://github.com/s3tools/s3cmd/archive/v$s3cmd_version.tar.gz
 tar xzf v$s3cmd_version.tar.gz
 
-s3cmd-$s3cmd_version/s3cmd sync --acl-public --progress site/content/ s3://$S3_BUCKET --bucket-location=EU
-
-
+s3cmd-$s3cmd_version/s3cmd sync --acl-public --progress site/output/ s3://$S3_BUCKET --bucket-location=EU
