1c20c52687065df5c6569fe8c29af3ba30eadb91
Patrick Debois <Patrick.Debois@jedi.be>
2014-12-28 12:06:55 +0100

    try to find the error for circleci & s3cmd

--- scripts-ci/s3_upload.sh
+++ scripts-ci/s3_upload.sh
@@ -16,7 +16,8 @@ echo "download s3cmd $s3cmd_version"
 curl -O -L https://github.com/s3tools/s3cmd/archive/v$s3cmd_version.tar.gz
 tar xzf v$s3cmd_version.tar.gz
 
-s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="application/javascript" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.js'
+s3cmd-$s3cmd_version/s3cmd sync --acl-public --progress --mime-type="application/javascript" site/output/ s3://$S3_BUCKET --bucket_location=EU --exclude '*.*' --include '*.js'
+#s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="application/javascript" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.js'
 #s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="text/css" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.css'
 #s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="text/html" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.html'
 #s3cmd-$s3cmd_version/s3cmd sync  --acl-public -M --progress site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.css' --exclude '*.js' --exclude '*.html'
