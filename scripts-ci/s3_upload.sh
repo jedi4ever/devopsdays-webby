fa259e9b3a636528d0eb5463d81cf27749bc22a6
Patrick Debois <Patrick.Debois@jedi.be>
2014-12-28 13:21:43 +0100

    try s3cmd rc1

--- scripts-ci/s3_upload.sh
+++ scripts-ci/s3_upload.sh
@@ -11,14 +11,14 @@ echo "[default]" > ~/.s3cfg
 echo "access_key=$S3_ACCESS_KEY" >> ~/.s3cfg
 echo "secret_key=$S3_SECRET_KEY" >> ~/.s3cfg
 
-s3cmd_version=1.5.0-beta1
+s3cmd_version=1.5.0-rc1
 echo "download s3cmd $s3cmd_version"
 curl -O -L https://github.com/s3tools/s3cmd/archive/v$s3cmd_version.tar.gz
 tar xzf v$s3cmd_version.tar.gz
 
-cd site/output
-../../s3cmd-$s3cmd_version/s3cmd sync --acl-public --progress --mime-type="application/javascript" --bucket-location=EU ./ s3://$S3_BUCKET  --exclude '*.*' --include '*.js'
-#s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="application/javascript" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.js'
+#cd site/output
+#../../s3cmd-$s3cmd_version/s3cmd sync --acl-public --progress --mime-type="application/javascript" --bucket-location=EU ./ s3://$S3_BUCKET  --exclude '*.*' --include '*.js'
+s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="application/javascript" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.js'
 #s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="text/css" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.css'
 #s3cmd-$s3cmd_version/s3cmd sync  --acl-public --progress --mime-type="text/html" site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.*' --include '*.html'
 #s3cmd-$s3cmd_version/s3cmd sync  --acl-public -M --progress site/output/ s3://$S3_BUCKET --bucket-location=EU --exclude '*.css' --exclude '*.js' --exclude '*.html'
